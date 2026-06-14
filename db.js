bce78c32ebd994510a9edc4285359caa29d813ea
samridhikc <samridhik023369@nec.edu.np>
2026-06-14 19:50:36 +0545

    Add MongoDB connection function

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Connected:', conn.connection.host);
+  } catch (error) {
+    console.error('MongoDB Error:', error.message);
+    process.exit(1);
+  }
+};
 
+module.exports = connectDB;
